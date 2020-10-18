dee709e56c8f0e26bddf9322ec839d1cab48e2e1
Alexandre Siqueira <alexandre1760@hotmail.com>
2020-10-18 12:57:57 -0300

    App 2

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository
+    ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customersExists = await this.customersRepository.findByEmail(email)
+
+    if(customersExists){
+      throw new AppError('Email ja exite')
+    }
+
+    const customer = await this.customersRepository.create({
+      email,
+      name,
+    })
+
+    return customer;
+
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,20 +21,43 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = await this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    })
+    await this.ormRepository.save(product)
+    return product
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
+
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      }
+    })
+
+    return product;
     // TODO
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsIds =  products.map(product => product.id)
+
+    const exitentProducts = await this.ormRepository.find({
+      where: {
+        id: In(productsIds)
+      }
+    })
+
+    return exitentProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
+    return this.ormRepository.save(products);
     // TODO
   }
 }
